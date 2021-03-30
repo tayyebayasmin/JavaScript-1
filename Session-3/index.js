340ea31c245989c2ac5d9b1e5a397139710d0607
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-03-30 15:00:03 +0500

    Session-3

---
+++ Session-3/index.js
@@ -0,0 +1,60 @@
+//splice 
+// var a = ['a' , 'b', 'c' ,'d']
+
+
+// a.splice(1,0, 'ahmad','hannan')
+// console.log(a)
+
+// var a = ['a' , 'b', 'c' ,'d']
+
+
+// a.splice(1,2, 'ahmad','hannan')
+// console.log(a)
+
+//slice
+
+// var a = ['a' , 'b', 'c' ,'d']
+
+
+// var y = a.slice(1,4)
+// console.log(y)
+
+
+// var a = ['a' , 'b', 'c' ,'d']
+
+
+// var y = a.slice(1)
+// console.log(y)
+
+// var a = 'session three'
+
+
+// var y = a.toUpperCase()
+// console.log(y)
+
+
+// var a = 'SESSION THREE'
+
+
+// var y = a.toLowerCase()
+// console.log(y)
+
+
+// var a = 'SESSION THREE'
+
+
+// var y = a.slice(3)
+// console.log(y)
+
+
+// var a = 'SESSION THREE'
+
+
+// var y = a.slice(3,9)
+// console.log(y)
+
+// var a = 'SESSION THREE'
+
+
+// var y = a.indexOf('THREE')
+// console.log(y)
