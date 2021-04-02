c72523b14ccc64ce5064fbb8d526de8449b8c083
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-04-02 15:38:50 +0500

    2 april

---
+++ Session-5/index.js
@@ -0,0 +1,4 @@
+for(var i = 5; i<10; i++){
+
+    console.log(i)
+}
---
+++ Session-6/index.js
@@ -0,0 +1,23 @@
+
+// function mul(params) {
+    
+//     var num  = 3 * params //* or divide
+//     return num
+// }
+// var a = mul(3)
+
+// console.log(a)
+
+// num1 = [1,2,3,4,5,6]
+
+// num(num1)
+
+// console.log(num1)
+
+
+// function num(num1) {
+    
+//     num1[1] = 'ahm'
+    
+// }
+
