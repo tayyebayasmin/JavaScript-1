87a90eaf5c0128f2b4153de1e2e9bbe0de45a691
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-04-08 15:10:46 +0500

    session-10

---
+++ Session-10/index.js
@@ -0,0 +1,38 @@
+// console.log(document)
+// console.log(document.all)
+
+// console.log(document.head.style)
+
+// console.log(document.URL)
+
+// console.log(document.title)
+
+// const a = document.getElementById('ses')
+
+// console.log(a.classList)
+
+// const a = document.getElementsByTagName('p')
+
+// console.log(a)
+
+//byQuery Seslecotr
+
+const a =  prompt('Enter any Number')
+
+if(parseInt(a)==4444){
+const a = document.querySelector('#pp') //only return first child
+
+a.style.background = 'pink'
+a.style.padding = '10px'
+a.style.borderRadius = '5px'
+a.innerText = 'Hasnat'
+a.style.color = 'White'
+console.log(a.innerContent)
+
+}
+else{
+    for(var b=0 ; b<4; b++)
+    alert('o bhai ap kidher')
+}
+
+
