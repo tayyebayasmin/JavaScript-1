d08e537a8c0b9742efaf5d6f846390ca80e1e1aa
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-06-01 20:17:32 +0500

    React JS Practice Start
    Created First  File

--- Ternary-Operator/index.js
+++ Ternary-Operator/index.js
@@ -48,23 +48,182 @@
 
 
 
-var testFunction = function(){
-    console.log("ahmad");
-    return false;
-    }
-    var testFunction2 = function(){
-    console.log("hannan");
-    return true;
-    }
+// var testFunction = function(){
+//     console.log("ahmad");
+//     return false;
+//     }
+//     var testFunction2 = function(){
+//     console.log("hannan");
+//     return true;
+//     }
     
     // var result = testFunction() || testFunction2();
     // //  prints true in console and never executes the testFunction2()
     // // result -> true
     
-    result = testFunction2() || testFunction();
+    // result = testFunction2() || testFunction();
 
     //i first value true ,,, then dont care about sencond
 
     //and 
 
     //first value true, then execute sencond  ==> || 
+
+
+
+
+
+    //--------------------------------
+
+
+
+   //------------------------- 30 May  ---------------------
+
+/// Husna
+
+
+//    let c = true;
+
+//    let f = ''
+
+//    c ? (console.log("computer is on"), 
+   
+//         f = prompt('which file yyou want to open?'),
+//         alert(`your file is in prcossing ${f}`)
+//             ): alert("plzz turn on ur comp")
+
+   
+
+
+//////////// Arrow Func
+
+
+// let a = () => 23
+
+// console.log(a())
+
+
+
+
+// let a = () => (1111111111111111 +
+//     576
+//     )
+
+// console.log(a())
+
+
+
+// let a = () => {
+
+    // let a = 'Ahmad'
+    // let b = "Hannan"
+    // let c = a + b
+//     console.log(c)
+
+//     return(c)
+// }
+
+// let complete_name = a();
+
+// console.log(complete_name)
+
+//---------------------------------
+
+
+// let a = (a,b) => {
+
+//     let c = a + b
+//     console.log(c)
+
+//     return(c)
+// }
+
+// let complete_name = a('Ahmad',"Hannan");
+
+// console.log(complete_name)
+
+//-------------------
+
+
+
+
+// let arrowFunc = c => {
+
+//     console.log(c)
+
+//     return(c)
+// }
+
+// let a = 'Ahmad'
+// let b = "Hannan"
+// let c = a + b
+
+// let complete_name = arrowFunc(c);
+
+
+
+
+// let arrowFunc = (a,b) => a + b 
+
+// let a = 'Ahmad'
+// let b = "Hannan"
+
+// let complete_name = arrowFunc(a,b);
+
+// console.log(complete_name)
+
+
+// let a = console.log("ahmad")
+
+// console.log(a)
+
+
+//-----------------------
+
+
+
+// function User(first, last) {
+//     this.firstName = first
+//     this.lastName = last
+//   }
+  
+  
+//   var user1 = new User("Jon", "Snow")
+//   console.log(user1)
+//   var user2 = new User("Ned", "Stark")
+//   console.log(user2)
+//   var user3 = new User("Jo", "Sno")
+//   console.log(user1)
+//   var user4 = new User("N", "St")
+//   console.log(user2)
+
+
+
+
+
+
+
+//  age = 50
+
+// function Person() {
+//     this.name = 'Jack',
+//     this.age = 25,
+//     this.sayName = function () {
+
+//         // this is accessible
+//         console.log(this.age);
+
+//         function innerFunc() {
+
+//             // this refers to the global object
+//             console.log(this.age);
+//             console.log(this);
+//         }
+
+//         innerFunc();
+
+//     }
+// }
+
+// let x = new Person();
+// x.sayName();
